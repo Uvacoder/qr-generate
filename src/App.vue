a60d3eb0580c9f2f9cd7652a1d8e05c57c726419
Sigve Hansen <sighan12@gmail.com>
2022-09-27 12:32:14 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Input, Button, Form, Flex } from '@sigveh/basic-ui'
+</script>
+
+<template>
+  <Form>
+    <Flex direction="column" align="end">
+      <Input placeholder="https://example.com" />
+      <Button theme="primary">Generate</Button>
+    </Flex>
+  </Form>
+</template>
+
+<style></style>
